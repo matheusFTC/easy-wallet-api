e7a52f604ef0fee8f688b509e81f660a92a542ff
Matheus Filipe Teixeira Chaves <matheus.02468@gmail.com>
2019-06-01 19:18:36 -0300

    Ajustada a restrinção de administrador.

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,12 @@ let router = express.Router();
 const controller = require('../controllers/user');
 
 const validate = require('../controllers/authentication').validate;
-const isAdministrator = require('../controllers/authorization').isAdministrator;
+const restrict = require('../controllers/authentication').restrict;
 
-router.get('/', validate, isAdministrator, controller.findAll);
+router.get('/', restrict, controller.findAll);
 router.get('/:_id', validate, controller.findById);
 router.post('/', controller.save);
 router.put('/:_id', validate, controller.save);
-router.delete('/:_id', validate, isAdministrator, controller.remove);
+router.delete('/:_id', restrict, controller.remove);
 
 module.exports = router;
