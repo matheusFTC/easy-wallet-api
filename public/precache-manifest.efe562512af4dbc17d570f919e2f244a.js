22eb7230b7154ed37d152c63dbd3ac7928a579b5
Matheus Filipe Teixeira Chaves <matheus.02468@gmail.com>
2019-07-11 19:31:46 -0300

    Atualizado o front-end da aplicação.

--- public/precache-manifest.efe562512af4dbc17d570f919e2f244a.js
+++ public/precache-manifest.efe562512af4dbc17d570f919e2f244a.js
@@ -4,11 +4,11 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.71c11578.js"
   },
   {
-    "revision": "5bff5057f1baaa134142",
-    "url": "/js/app.58215416.js"
+    "revision": "af2112949a7753b42d24",
+    "url": "/js/app.7dfc7b02.js"
   },
   {
-    "revision": "0da35b7770232827e5eca5c23f7e9f26",
+    "revision": "3d93d1e7415e532b1857e023057550ab",
     "url": "/index.html"
   },
   {
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/css/chunk-vendors.0ca6624a.css"
   },
   {
-    "revision": "5bff5057f1baaa134142",
+    "revision": "af2112949a7753b42d24",
     "url": "/css/app.6aeb3d08.css"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.488c4bc18e787f8711f11661142a9dfc.js"
+  "/precache-manifest.efe562512af4dbc17d570f919e2f244a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "easy-wallet-view"});
